f1ac2ee75f190539682622ab523a9bf79667761a
Krzysztof Królikowski <>
2022-04-23 14:51:18 +0200

    init

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+git push -f git@github.com:krzysztofkrolikowski/krzysztofkrolikowski.github.io.git main
+
+cd -
+
+rm -rf dist
